cf7eecdb91fd09367171013fe754db3cf6bef0cc
Davi Menezes <davidmenezes696@gmail.com>
2026-02-21 20:34:45 -0300

    feat: Add ngx-markdown with HttpClient loader and Katex support.

--- index.tsx
+++ index.tsx
@@ -2,8 +2,9 @@ import './src/styles.css';
 import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, provideZonelessChangeDetection } from '@angular/core';
+import { provideMarkdown } from 'ngx-markdown';
 import { AppComponent } from './src/app.component';
 import { APP_ROUTES } from './src/app.routes';
 import { authInterceptor } from './src/interceptors/auth.interceptor';
@@ -13,6 +14,7 @@ bootstrapApplication(AppComponent, {
     provideZonelessChangeDetection(),
     provideRouter(APP_ROUTES),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideMarkdown({ loader: HttpClient, katex: true }),
   ],
 }).catch(err => console.error(err));
 
